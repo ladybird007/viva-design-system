a0c7d832fe8ff3ba9e0dad60aed430f92a44ffd7
gorodnichaya-av <gorodnichaia.av@gmail.com>
2025-02-14 11:34:11 +0200

    Copy text function updates

--- js/copyText.js
+++ js/copyText.js
@@ -1,6 +1,17 @@
-function myFunction() {
-  const button = document.getElementById("copy-btn");
+function copyText(el) {
   const copyText = document.getElementById("copy-content").textContent;
-  
   navigator.clipboard.writeText(copyText);
+  el.classList.add('copied');
 }
+
+const copyBtns = document.querySelectorAll('code');
+copyBtns.forEach((btn) => {
+  const codeText = btn.textContent;
+  btn.addEventListener('click', () => {
+    copyBtns.forEach((btn) => btn.classList.remove('copied'));
+    navigator.clipboard.writeText(codeText);
+    btn.classList.add('copied');
+  });
+});
+
+
