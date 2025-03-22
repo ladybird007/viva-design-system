b9faf497b35272efd96261cfc6791f90d27da87f
gorodnichaya-av <gorodnichaia.av@gmail.com>
2025-03-22 14:08:05 +0200

    Pagination

---
+++ js/dropdown.js
@@ -0,0 +1,19 @@
+(function ($) {
+  $(document).ready(function(){
+
+    $('.js-dropdown-btn').on('click', function(e) {
+      e.stopPropagation();
+      if($(this).hasClass('opened')) {
+        $(this).next().stop(true,true).slideUp('slow'); 
+        $(this).removeClass('opened')
+      } else {
+        if($(this).hasClass('accordion-header')) {
+          $('.js-dropdown-btn').removeClass('opened');
+          $('.js-dropdown-content').slideUp('slow');
+        }
+        $(this).addClass('opened');
+        $(this).next().stop(true,true).slideDown('slow');
+      }
+    })
+  })
+})(jQuery)
