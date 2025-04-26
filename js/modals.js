53ea01326a49bb5e93666879beb802c5e4bae59a
gorodnichaya-av <gorodnichaia.av@gmail.com>
2025-04-26 10:06:52 +0300

    Cards not finished

--- js/modals.js
+++ js/modals.js
@@ -8,18 +8,29 @@
       $('body').css('overflow', 'hidden');
     });
 
+    $('.js-modal-video-btn').on('click', function(e) {
+      e.preventDefault();
+      const dataId = $(this).attr('data-id'),
+            dataSrc = $(this).attr('data-src');
+      $(dataId).fadeIn();
+      $(dataId).find('#js-modal-video').attr('src', dataSrc);
+      $('body').css('overflow', 'hidden');
+    });
+
     $('.js-modal-close').on('click', function(e) {
       e.preventDefault();
-      $(this).closest('.modal').fadeOut()
+      $(this).closest('.modal').fadeOut();
       $('body').css('overflow', 'auto');
+      $('#js-modal-video').attr('src', '');
     });
 
     $(".modal-overlay").on('click', function(event) {
       event.stopPropagation();
       let eTarget = event.target;
       if(!(eTarget.classList.contains('modal-container'))) {
-        $(this).closest('.modal').fadeOut()
+        $(this).closest('.modal').fadeOut();
         $('body').css('overflow', 'auto');
+        $('#js-modal-video').attr('src', '');
       } 
     });
   })
